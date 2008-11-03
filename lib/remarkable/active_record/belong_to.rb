901e7aa2f70f4e84b1573c9dfca5a831dd94002b
Carlos Brando <eduardobrando@gmail.com>
2008-11-03 15:50:47 -0200

    Refactoring belong_to matcher.

--- lib/remarkable/active_record/belong_to.rb
+++ lib/remarkable/active_record/belong_to.rb
@@ -6,33 +6,44 @@ class BelongTo
 
   def matches?(klass)
     @klass = klass
-
     @associations.each do |association|
-      describe klass do
-        it "should belong to #{association}" do
-          reflection = klass.reflect_on_association(association)
-          reflection.should_not be_nil
-          reflection.macro.should eql(:belongs_to)
+      @association = association
+      reflection = klass.reflect_on_association(association)
 
-          unless reflection.options[:polymorphic]
-            associated_klass = (reflection.options[:class_name] || association.to_s.camelize).constantize
-            fk = reflection.options[:foreign_key] || reflection.primary_key_name
-            klass.column_names.include?(fk.to_s).should be_true
-          end
-        end        
-      end      
+      unless reflection && reflection.macro == :belongs_to
+        @message = "#{klass.name} does not have any relationship to #{association}"
+        return false
+      end
+
+      unless reflection.options[:polymorphic]
+        associated_klass = (reflection.options[:class_name] || association.to_s.camelize).constantize
+        fk = reflection.options[:foreign_key] || reflection.primary_key_name
+
+        unless klass.column_names.include?(fk.to_s)
+          @message = "#{klass.name} does not have a #{fk} foreign key."
+          return false
+        end
+      end
     end
   end
 
+  def description
+    "belong to #{@associations.to_sentence}"
+  end
+
   def failure_message
-    "expected #{@klass.inspect} to belong_to #{@associations.inspect}, but it didn't"
+    @message || "expected #{@klass.inspect} to belong_to #{@association.inspect}, but it didn't"
   end
 
   def negative_failure_message
-    "expected #{@klass.inspect} not to belong_to #{@associations.inspect}, but it did"
+    "#{@klass} belong to #{@associations.to_sentence}"
   end
 end
 
+# Ensure that the belongs_to relationship exists.
+#
+#   it { Model.should belong_to(:parent, :parent) }
+#
 def belong_to(*associations)
   BelongTo.new(*associations)
 end
