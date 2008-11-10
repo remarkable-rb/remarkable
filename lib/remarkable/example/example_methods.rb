27495102292981228e20c4a54bd27c1d6472bc0e
Carlos Brando <eduardobrando@gmail.com>
2008-11-10 19:45:54 -0200

    No conflict with Rspec.

--- lib/remarkable/example/example_methods.rb
+++ lib/remarkable/example/example_methods.rb
@@ -2,15 +2,16 @@ module Spec
   module Example
     module ExampleMethods
       def should(matcher)
-        subject.should(matcher)
+        remarkable_subject.should(matcher)
       end
 
       def should_not(matcher)
-        subject.should_not(matcher)
+        remarkable_subject.should_not(matcher)
       end
 
-      def subject
-        @subject ||= self.class.described_type
+      def remarkable_subject
+        @remarkable_subject = subject if self.respond_to?(:subject)
+        @remarkable_subject ||= self.class.described_type
       end
     end
   end
