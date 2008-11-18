0d11ab67fc934e0503567ac3df6d428855e1505d
Carlos Brando <eduardobrando@gmail.com>
2008-11-18 12:56:17 -0200

    Change version to 1.1.0.

--- lib/remarkable.rb
+++ lib/remarkable.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Remarkable
-  VERSION = '1.0.0'
+  VERSION = '1.1.0'
 end
 
 require "spec"
