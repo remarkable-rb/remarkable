62de67a165df0c3e811e94a55b9508ea926f6b10
José Valim <jose.valim@gmail.com>
2009-02-08 17:07:06 +0100

    Added support for pending macros (xvalidate_presence_of(:name))

--- lib/remarkable/active_record/macros.rb
+++ lib/remarkable/active_record/macros.rb
@@ -5,9 +5,11 @@ module Remarkable # :nodoc:
 
       def method_missing_with_remarkable(method_id, *args, &block)
         if method_id.to_s =~ /^should_not_(.*)/
-          should_not_method_missing($1.to_sym, *args)
+          should_not_method_missing($1, *args)
         elsif method_id.to_s =~ /^should_(.*)/
-          should_method_missing($1.to_sym, *args)
+          should_method_missing($1, *args)
+        elsif method_id.to_s =~ /^xshould_(not_)?(.*)/
+          pending_method_missing($2, $1, *args)
         else
           method_missing_without_remarkable(method_id, *args, &block)
         end
@@ -17,21 +19,34 @@ module Remarkable # :nodoc:
       private
 
       def should_not_method_missing(method, *args)
-        matcher = send(method, *args).negative
-        matcher.table_name = self.described_type.table_name if matcher.respond_to?('table_name=')
+        matcher = create_and_configure_matcher(method, *args).negative
         it "should not #{matcher.description}" do
           assert_rejects(matcher, model_class)
         end
       end
 
       def should_method_missing(method, *args)
-        matcher = send(method, *args)
-        matcher.table_name = self.described_type.table_name if matcher.respond_to?('table_name=')
+        matcher = create_and_configure_matcher(method, *args)
         it "should #{matcher.description}" do
           assert_accepts(matcher, model_class)
         end
       end
-      
+
+      def pending_method_missing(method, negative, *args)
+        matcher = create_and_configure_matcher(method, *args)
+        matcher.negative if negative
+        description = matcher.description
+        xit "should #{'not ' if negative}#{description}"
+      rescue
+        xit "should #{'not ' if negative}#{method.to_s.gsub('_',' ')}"
+      end
+
+      def create_and_configure_matcher(method, *args)
+        matcher = send(method, *args)
+        matcher.table_name = self.described_type.table_name if matcher.respond_to?('table_name=')
+        matcher
+      end
+
     end
   end
 end
--- lib/remarkable/controller/macros.rb
+++ lib/remarkable/controller/macros.rb
@@ -35,9 +35,11 @@ module Remarkable # :nodoc:
 
       def method_missing_with_remarkable(method_id, *args, &block)
         if method_id.to_s =~ /^should_not_(.*)/
-          should_not_method_missing($1.to_sym, *args)
+          should_not_method_missing($1, *args)
         elsif method_id.to_s =~ /^should_(.*)/
-          should_method_missing($1.to_sym, *args)
+          should_method_missing($1, *args)
+        elsif method_id.to_s =~ /^xshould_(not_)?(.*)/
+          pending_method_missing($2, $1, *args)
         else
           method_missing_without_remarkable(method_id, *args, &block)
         end
@@ -61,6 +63,16 @@ module Remarkable # :nodoc:
           assert_rejects(matcher, model_class)
         end
       end
+
+      def pending_method_missing(method, negative, *args)
+        matcher = send(method, *args)
+        matcher.negative if negative
+        description = matcher.description
+        xit "should #{'not ' if negative}#{description}"
+      rescue
+        xit "should #{'not ' if negative}#{method.to_s.gsub('_',' ')}"
+      end
+
     end
   end
 end
