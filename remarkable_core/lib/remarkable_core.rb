60e39d3d545d372ac9f139db0e0007087c6bf0c2
José Valim <jose.valim@gmail.com>
2009-02-18 15:23:31 +0100

    Added I18n basic files.

--- remarkable_core/lib/remarkable_core.rb
+++ remarkable_core/lib/remarkable_core.rb
@@ -1,3 +1,5 @@
+dir = File.dirname(__FILE__)
+
 module Remarkable
   # Helper that includes required Remarkable modules into the given klass.
   def self.include_matchers!(base, klass)
@@ -8,13 +10,31 @@ module Remarkable
     klass.send :extend,  base::Matchers if defined?(base::Matchers)
     klass.send :extend,  base::Macros   if defined?(base::Macros)
   end
+
+  # Add locale files to I18n
+  def self.add_locale(*locales)
+    I18n.backend.load_translations *locales
+  end
 end
 
-dir = File.dirname(__FILE__)
+# Load I18n
+unless Object.const_defined?('I18n')
+  begin
+    require 'i18n'
+  rescue Exception => e
+    require 'rubygems'
+    # TODO Move to i18n gem as soon as it gets updated
+    gem 'josevalim-i18n'
+    require 'i18n'
+  end
+end
+
+# Add Remarkable default locale file
+Remarkable.add_locale File.join(dir, '..', 'locale', 'en.yml')
+
+# Load core files
 require File.join(dir, 'remarkable_core', 'version')
 require File.join(dir, 'remarkable_core', 'core_ext', 'array')
-
 require File.join(dir, 'remarkable_core', 'dsl')
 require File.join(dir, 'remarkable_core', 'base')
 require File.join(dir, 'remarkable_core', 'macros')
-
