a686107946fcee992394616b128b692f92018561
José Valim <jose.valim@gmail.com>
2009-03-04 15:04:40 +0100

    Adding attribute and good_value to inflector to prevent those rules from being overwritten.

--- remarkable_activerecord/lib/remarkable_activerecord.rb
+++ remarkable_activerecord/lib/remarkable_activerecord.rb
@@ -13,6 +13,7 @@ end
 dir = File.dirname(__FILE__)
 require File.join(dir, 'remarkable_activerecord', 'base')
 require File.join(dir, 'remarkable_activerecord', 'human_names')
+require File.join(dir, 'remarkable_activerecord', 'inflections')
 
 # Add locale
 Remarkable.add_locale File.join(dir, '..', 'locale', 'en.yml')
