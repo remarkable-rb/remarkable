0bb16d8fc5b0ff8d7dc5c4304a6bb3ee8f0137d1
José Valim <jose.valim@gmail.com>
2009-03-17 02:17:14 +0100

    Releasing new gem (Remarkable 2.3.0)

--- lib/remarkable/active_record/macros/validations/protect_attributes_matcher.rb
+++
@@ -1,8 +0,0 @@
-module Remarkable # :nodoc:
-  module ActiveRecord # :nodoc:
-    module Matchers # :nodoc:
-      class ProtectAttributes < Remarkable::Matcher::Base
-      end
-    end
-  end
-end
