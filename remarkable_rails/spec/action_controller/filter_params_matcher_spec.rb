7b9f4b7a8a71ad770d5c22b6b11dfe7b20196f8c
José Valim <jose.valim@gmail.com>
2009-03-20 14:58:37 +0100

    Added filter_params matcher and updated CHANGELOG.

---
+++ remarkable_rails/spec/action_controller/filter_params_matcher_spec.rb
@@ -0,0 +1,65 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe 'filter_params' do
+  include FunctionalBuilder
+
+  describe 'messages' do
+    before(:each) do
+      @controller = define_controller :Projects do
+        filter_parameter_logging :password
+      end.new
+
+      @matcher = filter_params(:user)
+    end
+
+    it 'should contain a description message' do
+      @matcher.description.should == 'filter parameters user from log'
+    end
+
+    it 'should set respond_to_filter_params? message' do
+      @controller.class_eval "undef_method :filter_parameters"
+      @matcher.matches?(@controller)
+      @matcher.failure_message.should == 'Expected controller to respond to filter_parameters (controller is not filtering any parameter)'
+    end
+
+    it 'should set is_filtered? message' do
+      @matcher.matches?(@controller)
+      @matcher.failure_message.should == 'Expected user to be filtered'
+    end
+  end
+
+  describe 'filtering parameter' do
+    before(:each) do 
+      @controller = define_controller :Projects do
+        filter_parameter_logging :password
+      end.new
+
+      self.class.subject { @controller }
+    end
+
+    should_filter_params
+    should_filter_params(:password)
+    should_not_filter_params(:user)
+
+    it { should filter_params }
+    it { should filter_params(:password) }
+    it { should_not filter_params(:user) }
+  end
+
+  describe 'not filtering parameter' do
+    before(:each) do 
+      @controller = define_controller(:Projects).new
+
+      self.class.subject { @controller }
+    end
+
+    should_not_filter_params
+    should_not_filter_params(:password)
+    should_not_filter_params(:user)
+
+    it { should_not filter_params }
+    it { should_not filter_params(:user) }
+    it { should_not filter_params(:password) }
+  end
+
+end
