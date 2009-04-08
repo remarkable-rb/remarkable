033b9c5dc309f2631488217cb23a9dd5f5a09586
José Valim <jose.valim@gmail.com>
2009-04-08 15:09:04 +0200

    Added have_scope matcher.

---
+++ remarkable_activerecord/lib/remarkable_activerecord/matchers/have_scope_matcher.rb
@@ -0,0 +1,78 @@
+module Remarkable
+  module ActiveRecord
+    module Matchers
+      class HaveScopeMatcher < Remarkable::ActiveRecord::Base
+        arguments :scope_name
+        assertions :is_scope?, :options_matches?
+
+        optionals :with, :select, :conditions, :order, :limit, :offset
+
+        protected
+
+          def is_scope?
+            @scope_object = if @options[:with]
+              subject_class.send(@scope_name, *@options[:with])
+            else
+              subject_class.send(@scope_name)
+            end
+
+            @scope_object.class == ::ActiveRecord::NamedScope::Scope
+          end
+
+          def options_matches?
+            @options.empty? || @scope_object.proxy_options == @options.except(:with)
+          end
+
+          def interpolation_options
+            { :options => @options.except(:with).inspect,
+              :actual  => (@scope_object ? @scope_object.proxy_options.inspect : '{}')
+            }
+          end
+
+      end
+
+      # Ensures that the model has a method named scope that returns a NamedScope
+      # object with the supplied proxy options.
+      #
+      # == Options
+      #
+      # * <tt>with</tt> - Options to be sent to the named scope
+      #
+      # And all other options that the named scope would pass on to find.
+      #
+      # == Examples
+      # 
+      #   it { should have_scope(:visible, :conditions => {:visible => true}) }
+      #   it { should have_scope(:visible).conditions(:visible => true) }
+      #
+      # Passes for
+      #
+      #   named_scope :visible, :conditions => {:visible => true}
+      #
+      # Or for
+      #
+      #   def self.visible
+      #     scoped(:conditions => {:visible => true})
+      #   end
+      #
+      # You can test lambdas or methods that return ActiveRecord#scoped calls:
+      #
+      #   it { should have_scope(:recent, :with => 5) }
+      #   it { should have_scope(:recent, :with => 1) }
+      #
+      # Passes for
+      #
+      #   named_scope :recent, lambda {|c| {:limit => c}}
+      #
+      # Or for
+      #
+      #   def self.recent(c)
+      #     scoped(:limit => c)
+      #   end
+      #
+      def have_scope(*args)
+        HaveScopeMatcher.new(*args)
+      end
+    end
+  end
+end
---
+++ remarkable_activerecord/spec/have_scope_matcher_spec.rb
@@ -0,0 +1,69 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe 'have_scope' do
+  include ModelBuilder
+
+  before(:each) do
+    @model = define_model :product, :title => :string, :category => :string do
+      named_scope :recent, :order => 'created_at DESC'
+      named_scope :latest, lambda {|c| {:limit => c}}
+
+      def self.beginning(c)
+        scoped(:offset => c)
+      end
+
+      def self.null
+        nil
+      end
+    end
+  end
+
+  describe 'messages' do
+
+    it 'should contain a description' do
+      @matcher = have_scope(:title)
+      @matcher.description.should == 'have to scope itself to {} when :title is called'
+
+      @matcher.with(1)
+      @matcher.description.should == 'have to scope itself to {} when :title is called with 1 as argument'
+    end
+
+    it 'should set is_scope? missing message' do
+      @matcher = have_scope(:null)
+      @matcher.matches?(@model)
+      @matcher.failure_message.should == 'Expected :null when called on Product return a ActiveRecord::NamedScope::Scope object'
+    end
+
+    it 'should set has_options? missing message' do
+      @matcher = have_scope(:recent, :conditions => {:special => true})
+      @matcher.matches?(@model)
+      @matcher.failure_message.should == 'Expected :recent when called on Product scope to {:conditions=>{:special=>true}}, got {:order=>"created_at DESC"}'
+    end
+
+  end
+
+  describe 'matchers' do
+    it { should have_scope(:recent) }
+    it { should have_scope(:recent, :order => 'created_at DESC') }
+
+    it { should have_scope(:latest,    :with => 10, :limit => 10) }
+    it { should have_scope(:beginning, :with => 10, :offset => 10) }
+
+    it { should_not have_scope(:null) }
+    it { should_not have_scope(:latest,    :with => 5, :limit => 10) }
+    it { should_not have_scope(:beginning, :with => 5, :offset => 10) }
+  end
+
+  describe 'macros' do
+    should_have_scope :recent
+    should_have_scope :recent, :order => 'created_at DESC'
+
+    should_have_scope :latest,    :with => 10, :limit => 10
+    should_have_scope :beginning, :with => 10, :offset => 10
+
+    should_not_have_scope :null
+    should_not_have_scope :latest,    :with => 5, :limit => 10
+    should_not_have_scope :beginning, :with => 5, :offset => 10
+  end
+end
+
