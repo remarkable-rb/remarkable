45b82be6b38a736098c3576b1cff1a43656fb7df
José Valim <jose.valim@gmail.com>
2009-04-08 14:12:05 +0200

    Added have_readonly_attributes (three to go!)

---
+++ remarkable_activerecord/lib/remarkable_activerecord/matchers/have_readonly_attributes_matcher.rb
@@ -0,0 +1,28 @@
+module Remarkable # :nodoc:
+  module ActiveRecord # :nodoc:
+    module Matchers # :nodoc:
+      class HaveReadonlyAttributesMatcher < Remarkable::ActiveRecord::Base
+        arguments :collection => :attributes, :as => :attribute
+        collection_assertions :is_readonly?
+
+        private
+
+          def is_readonly?
+            readonly = subject_class.readonly_attributes || []
+            return readonly.include?(@attribute.to_s), :actual => readonly.to_a.inspect
+          end
+      end
+
+      # Ensures that the attribute cannot be changed once the record has been
+      # created.
+      #
+      # == Examples
+      #
+      #   it { should have_readonly_attributes(:password, :admin_flag) }
+      #
+      def have_readonly_attributes(*attributes)
+        HaveReadonlyAttributesMatcher.new(*attributes).spec(self)
+      end
+    end
+  end
+end
---
+++ remarkable_activerecord/spec/have_readonly_attributes_matcher_spec.rb
@@ -0,0 +1,47 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe 'have_readonly_attributes' do
+  include ModelBuilder
+
+  def define_and_validate(options={})
+    @model = define_model :product, :title => :string, :category => :string do
+      attr_readonly :title, :category   if options[:readonly] == true
+      attr_readonly *options[:readonly] if options[:readonly].is_a?(Array)
+    end
+
+    have_readonly_attributes(:title, :category)
+  end
+
+  describe 'messages' do
+
+    it 'should contain a description' do
+      @matcher = have_readonly_attributes(:title, :category)
+      @matcher.description.should == 'make title and category read-only'
+    end
+
+    it 'should set is_readonly? missing message' do
+      @matcher = define_and_validate(:readonly => [:another])
+      @matcher.matches?(@model)
+      @matcher.failure_message.should == 'Expected Product to make title read-only, got ["another"]'
+    end
+
+  end
+
+  describe 'matchers' do
+    it { should define_and_validate(:readonly => true) }
+    it { should_not define_and_validate(:readonly => false) }
+    it { should_not define_and_validate(:accessible => [:another]) }
+  end
+
+  describe 'macros' do
+    before(:each){ define_and_validate(:readonly => true) }
+
+    should_have_readonly_attributes :title
+    should_have_readonly_attributes :category
+    should_have_readonly_attributes :title, :category
+
+    should_not_have_readonly_attributes :another
+    should_not_have_readonly_attributes :title, :another
+  end
+end
+
