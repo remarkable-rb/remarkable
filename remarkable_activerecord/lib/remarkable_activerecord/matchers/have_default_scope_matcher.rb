c2c232ffad0892a4864dd1803d56aaf00dcf0e00
José Valim <jose.valim@gmail.com>
2009-04-23 22:24:39 +0200

    Have default scope matcher [#38 status:resolved]

---
+++ remarkable_activerecord/lib/remarkable_activerecord/matchers/have_default_scope_matcher.rb
@@ -0,0 +1,67 @@
+module Remarkable
+  module ActiveRecord
+    module Matchers
+      class HaveDefaultScopeMatcher < Remarkable::ActiveRecord::Base #:nodoc:
+        arguments
+        assertions :options_match?
+
+        optionals :select, :conditions, :join, :include, :group, :having, :order, :limit, :offset
+
+        protected
+
+          def options_match?
+            default_scope.include?(@options)
+          end
+
+          def default_scope
+            @default_scope ||= if @subject
+              scopes = @subject.class.default_scoping || []
+              scopes.map!{ |s| s[:find] }
+            else
+              []
+            end
+          end
+
+          def interpolation_options
+            { :options => @options.inspect, :actual => default_scope.inspect }
+          end
+
+      end
+
+      # Ensures that the model has a default scope with the given options.
+      #
+      # == Options
+      #
+      # All options that the default scope would pass on to find: select, conditions,
+      # join, include, group, having, order, limit e offset.
+      #
+      # == Examples
+      #
+      #   it { should have_default_scope(:conditions => {:visible => true}) }
+      #   it { should have_default_scope.conditions(:visible => true) }
+      #
+      # Passes for:
+      #
+      #   default_scope :conditions => { :visible => true }
+      #
+      # If you set two different default scopes, you have to spec them
+      # separatedly. Given the scopes:
+      #
+      #   default_scope :conditions => { :visible => true }
+      #   default_scope :conditions => { :published => true }
+      #
+      # Then we have the matchers:
+      #
+      #   should_have_default_scope :conditions => { :visible => true }   # Passes
+      #   should_have_default_scope :conditions => { :published => true } # Passes
+      #
+      #   should_have_default_scope :conditions => { :published => true,
+      #                                               :visible => true }  # Fails
+      #
+      def have_default_scope(*args)
+        HaveDefaultScopeMatcher.new(*args).spec(self)
+      end
+
+    end
+  end
+end
---
+++ remarkable_activerecord/spec/have_default_scope_matcher_spec.rb
@@ -0,0 +1,52 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+if RAILS_VERSION == '2.3.2'
+  describe 'have_default_scope' do
+    include ModelBuilder
+
+    before(:each) do
+      @model = define_model :product, :published => :boolean do
+        default_scope :order => 'created_at DESC'
+        default_scope :conditions => { :published => true }
+      end
+    end
+
+    describe 'messages' do
+
+      it 'should contain a description' do
+        @matcher = have_default_scope(:conditions => {:special => true})
+        @matcher.description.should == 'have a default scope with {:conditions=>{:special=>true}}'
+      end
+
+      it 'should set options_match? message' do
+        @matcher = have_default_scope(:conditions => {:special => true})
+        @matcher.matches?(@model)
+        @matcher.failure_message.should == 'Expected default scope with {:conditions=>{:special=>true}}, got [{:order=>"created_at DESC"}, {:conditions=>{:published=>true}}]'
+      end
+
+    end
+
+    describe 'matchers' do
+      it { should have_default_scope(:order => 'created_at DESC') }
+      it { should have_default_scope(:conditions => { :published => true }) }
+
+      it { should_not have_default_scope(:order => 'created_at ASC') }
+      it { should_not have_default_scope(:conditions => { :published => false }) }
+      it { should_not have_default_scope(:conditions => { :published => true }, :order => 'created_at DESC') }
+
+      describe 'when the model has no default scope' do
+        before(:each){ @model = define_model(:task) }
+        it { @model.should_not have_default_scope }
+      end
+    end
+
+    describe 'macros' do
+      should_have_default_scope :order => 'created_at DESC'
+      should_have_default_scope :conditions => { :published => true }
+
+      should_not_have_default_scope :order => 'created_at ASC'
+      should_not_have_default_scope :conditions => { :published => false }
+      should_not_have_default_scope :conditions => { :published => true }, :order => 'created_at DESC'
+    end
+  end
+end
