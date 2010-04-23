4639089cf5de78e059acad23f97e2f7c44eccb93
Ho-Sheng Hsiao <hosh@isshen.com>
2010-04-23 19:08:01 -0400

    [ARCH/AM] Reorgnized lib structure for remarkable_activemodel

---
+++ remarkable_activemodel/lib/remarkable/active_model.rb
@@ -0,0 +1,28 @@
+# Load Remarkable
+unless Object.const_defined?('Remarkable')
+  begin
+    require 'remarkable'
+  rescue LoadError
+    require 'rubygems'
+    gem 'remarkable'
+    require 'remarkable'
+  end
+end
+
+# Load Remarkable ActiveModel files
+dir = File.dirname(__FILE__)
+require File.join(dir, 'active_model', 'base')
+
+# Add locale
+Remarkable.add_locale File.join(dir, '..', '..', 'locale', 'en.yml')
+
+# Add matchers
+Dir[File.join(dir, 'active_model', 'matchers', '*.rb')].each do |file|
+  require file
+end
+
+# By default, ActiveModel matchers are not included in any example group.
+# The responsibility for this is RemarkableRails. If you are using ActiveModel
+# without Rails, put the line below in your spec_helper to include ActiveModel
+# matchers into rspec globally.
+# Remarkable.include_matchers!(Remarkable::ActiveModel, Rspec::Example::ExampleGroup)
--- remarkable_activemodel/lib/remarkable_activemodel.rb
+++ remarkable_activemodel/lib/remarkable_activemodel.rb
@@ -1,28 +1,5 @@
-# Load Remarkable
-unless Object.const_defined?('Remarkable')
-  begin
-    require 'remarkable'
-  rescue LoadError
-    require 'rubygems'
-    gem 'remarkable'
-    require 'remarkable'
-  end
-end
-
 # Load Remarkable ActiveModel files
 dir = File.dirname(__FILE__)
-require File.join(dir, 'remarkable_activemodel', 'base')
+require File.join(dir, 'remarkable', 'active_model')
 
-# Add locale
-Remarkable.add_locale File.join(dir, '..', 'locale', 'en.yml')
-
-# Add matchers
-Dir[File.join(dir, 'remarkable_activemodel', 'matchers', '*.rb')].each do |file|
-  require file
-end
-
-# By default, ActiveModel matchers are not included in any example group.
-# The responsibility for this is RemarkableRails. If you are using ActiveModel
-# without Rails, put the line below in your spec_helper to include ActiveModel
-# matchers into rspec globally.
-# Remarkable.include_matchers!(Remarkable::ActiveModel, Rspec::Example::ExampleGroup)
+Remarkable.include_matchers!(Remarkable::ActiveModel, Rspec::Core::ExampleGroup)
